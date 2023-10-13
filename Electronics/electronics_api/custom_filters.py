b8b882c8e6aead1f336d662e7a6bee19027b338f
kolyamba <n_selivonchik@rocketdata.io>
2023-10-13 08:27:15 +0300

    Фильтр по средней задолженности. - Добавил фильтр по средней задолженности (фильтры можно комбинировать)

--- Electronics/electronics_api/custom_filters.py
+++ Electronics/electronics_api/custom_filters.py
@@ -1,10 +1,18 @@
-from django_filters import (
-    FilterSet,
-    AllValuesFilter,
-)
+from django_filters import FilterSet, AllValuesFilter, ChoiceFilter
+from statistics import mean
 
 
 class CompanyFilter(FilterSet):
     country = AllValuesFilter(
         field_name="contact_id__address_id__country_code", label="Country"
     )
+    debt = ChoiceFilter(
+        choices=(("true", "Company with debt"),),
+        label="Company with debt",
+        method="filter_company_debt",
+        # widget=django_filters.widgets.LinkWidget,
+    )
+
+    def filter_company_debt(self, queryset, name, value):
+        average_debt = mean([company.arrears.amount for company in queryset])
+        return queryset.filter(arrears__gte=average_debt)
