5488989fd17af2e177a731afc58f00f62f67692a
kolyamba <n_selivonchik@rocketdata.io>
2023-10-18 23:29:23 +0300

    Добавил Docker. - Создал Dockerfile - Прописал docker-compose - создал два image (для приложения и для базы данных postgres)

---
+++ Dockerfile
@@ -0,0 +1,18 @@
+FROM python:3.11-alpine3.18
+
+COPY Pipfile ./
+COPY Electronics /Electronics
+WORKDIR /Electronics
+EXPOSE 8000
+
+RUN apk add postgresql-client build-base postgresql-dev
+
+
+RUN python -m pip install --upgrade pip
+
+RUN pip install pipenv && pipenv lock && pipenv install --dev --system --deploy
+
+RUN adduser --disabled-password electronics-user
+
+
+USER electronics-user
--- Electronics/Electronics/settings.py
+++ Electronics/Electronics/settings.py
@@ -83,11 +83,11 @@ WSGI_APPLICATION = "Electronics.wsgi.application"
 DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.postgresql",
-        "NAME": os.getenv("DB_NAME"),
-        "USER": os.getenv("DB_USER"),
-        "PASSWORD": os.getenv("DB_PASSWORD"),
-        "HOST": os.getenv("DB_HOST"),
-        "PORT": os.getenv("DB_PORT"),
+        "NAME": os.environ.get("DB_NAME"),
+        "HOST": os.environ.get("DB_HOST"),
+        # "PORT": os.environ.get("DB_HOST"),
+        "USER": os.environ.get("DB_USER"),
+        "PASSWORD": os.environ.get("DB_PASSWORD"),
     }
 }
 
